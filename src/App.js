650e50a8c75e4eabc4c8cb92faa2a187ed607896
Oliver Weber <oliver@mondough.io>
2019-05-06 16:20:40 +0200

    quick solution

--- src/App.js
+++ src/App.js
@@ -6,18 +6,37 @@ import TrackListItem from "./Components/Track-List-Item";
 
 class App extends React.Component {
   state = {
-    tracks: null
+    tracks: null,
+    searchTerm: ""
+  };
+
+  onSearchInputChange = e => {
+    e.persist();
+    const searchTerm = e.target.value;
+    this.setState({ searchTerm }, () => {
+      if (searchTerm.length > 2) {
+        this.searchForMusic();
+      }
+    });
+  };
+
+  searchForMusic = (searchTerm = this.state.searchTerm) => {
+    console.log("Searching ", searchTerm);
+    fetch(`https://dci-fbw12-search-itunes.now.sh/?term=${searchTerm}`)
+      .then(response => response.json())
+      .then(data => this.setState({ tracks: data.results }));
   };
 
   render() {
-    const { tracks } = this.state;
+    const { tracks, searchTerm } = this.state;
     return (
       <div className="App">
-        <SearchBar />
+        <SearchBar
+          onSearchInputChangeHandler={this.onSearchInputChange}
+          searchTerm={searchTerm}
+        />
         {tracks ? (
-          <TrackList>
-            <TrackListItem />
-          </TrackList>
+          <TrackList tracks={tracks} />
         ) : (
           <div>Search to find music</div>
         )}
