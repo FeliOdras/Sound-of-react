d28aad893ac585b1e3e825ed61ccfd52f3fc32d6
FeliOdras <ariane.feldmann@odras.de>
2019-05-07 13:07:57 +0200

    Adjusted style of serach input bar

--- src/Components/Search-Bar.js
+++ src/Components/Search-Bar.js
@@ -3,7 +3,7 @@ import React from "react";
 const SearchBar = props => {
   return (
     <input
-      className="border-0 ml-1 bg-transparent font-amatic"
+      className="ml-1 bg-transparent font-amatic border-bottom-2 border-left-0 border-right-0 border-top-0 border-dark text-center"
       value={props.searchTerm}
       onChange={props.onSearchInputChangeHandler}
       placeholder="Enter your search term"
